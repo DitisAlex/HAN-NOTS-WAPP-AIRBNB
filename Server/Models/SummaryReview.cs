33cb294542c1dd9a96c79b8d23c1fb378cf741ed
Alex Cheng <alexcheng0314@gmail.com>
2022-05-13 12:17:29 +0200

    Added API support

---
+++ Server/Models/SummaryReview.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Server.Models
+{
+    public partial class SummaryReview
+    {
+        public int ListingId { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
