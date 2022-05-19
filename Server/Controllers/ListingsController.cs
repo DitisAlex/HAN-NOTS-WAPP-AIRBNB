8ca220ed6b93e0cadf8a9d9f78504539e8b4c496
Alex Cheng <alexcheng0314@gmail.com>
2022-05-19 11:10:58 +0200

    Removed useless routes

--- Server/Controllers/ListingsController.cs
+++ Server/Controllers/ListingsController.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Server.Models;
 
@@ -47,91 +42,6 @@ namespace Server.Controllers
             }
 
             return listing;
-        }
-
-        // PUT: api/Listings/2818
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutListing(int id, Listing listing)
-        {
-            if (id != listing.Id)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(listing).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!ListingExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
-
-        // POST: api/Listings
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        public async Task<ActionResult<Listing>> PostListing(Listing listing)
-        {
-          if (_context.Listings == null)
-          {
-              return Problem("Entity set 'AirBNBContext.Listings'  is null.");
-          }
-            _context.Listings.Add(listing);
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateException)
-            {
-                if (ListingExists(listing.Id))
-                {
-                    return Conflict();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return CreatedAtAction("GetListing", new { id = listing.Id }, listing);
-        }
-
-        // DELETE: api/Listings/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteListing(int id)
-        {
-            if (_context.Listings == null)
-            {
-                return NotFound();
-            }
-            var listing = await _context.Listings.FindAsync(id);
-            if (listing == null)
-            {
-                return NotFound();
-            }
-
-            _context.Listings.Remove(listing);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
-        }
-
-        private bool ListingExists(int id)
-        {
-            return (_context.Listings?.Any(e => e.Id == id)).GetValueOrDefault();
         }
     }
 }
