dd659b706e273cac78f792541320cd0b76aedc38
Alex Cheng <alexcheng0314@gmail.com>
2022-05-20 11:08:27 +0200

    Added CORS policy

--- Server/Program.cs
+++ Server/Program.cs
@@ -2,9 +2,18 @@
 using Server.Models;
 using Server.Repositories;
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("http://localhost:3000");
+                      });
+});
 
 builder.Services.AddControllers();
 
@@ -15,14 +24,11 @@ builder.Services.AddDbContext<AirBNBContext>(options => {
 
 builder.Services.AddScoped<IListingsRepository, ListingsRepository>();
 
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -30,6 +36,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseAuthorization();
 
